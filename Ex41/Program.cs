a693f25ef7d7fe594e340b9316abb9cffd6e7e2f
Olga Lobanova <arapova@list.ru>
2022-10-09 16:14:18 +0300

    Задача 41

---
+++ Ex41/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+ 
+int numberCount = Convert.ToInt32(Console.ReadLine()!);
+int[] numberArray = new int[numberCount];
+ 
+for (int i = 0; i < numberArray.Length; i++)
+{
+    numberArray[i] = Convert.ToInt32(Console.ReadLine()!);
+}
+ 
+Console.WriteLine(CountPositiveNumbers(numberArray));
+ 
+ 
+int CountPositiveNumbers(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
